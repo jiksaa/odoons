3cc2d198947e91c51a6ef1a4dcb10cba62039208
Jiksaa <jonathan.beersaerts1993@gmail.com>
2021-10-04 23:50:39 +0200

    Pin PyYAML version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
     ],
     python_requires='>=3.6',
     install_requires=[
-        'PyYAML'
+        'PyYAML>=5.1'
     ],
     entry_points={
         'console_scripts': ['odoons = odoons.core:main']
